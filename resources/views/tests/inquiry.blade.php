d79d4e752412117f3a8dba053620fb720443fcf5
JIKim0303 <j_inoue9133@outlook.com>
2023-11-07 23:10:20 +0900

    updatedon7th

--- resources/views/tests/inquiry.blade.php
+++ resources/views/tests/inquiry.blade.php
@@ -5,8 +5,39 @@
 @section('content')
     <div class="parent">
         <img class="dogback" src="{{ asset('pics/footstamp.jpg') }}">
-            <div class="child">
+            <!-- <div class="child">
             <iframe class="gform" src="https://docs.google.com/forms/d/e/1FAIpQLSecHqkTupvGHeyLyBqlQmaEUm7dkGfP9kdeFs8QKtS51TG3sw/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+            </div> -->
+
+        <div class="child" id="formWrapper">
+            <form class="gform" name="gForm" action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSecHqkTupvGHeyLyBqlQmaEUm7dkGfP9kdeFs8QKtS51TG3sw/formResponse" method="post" target="dummyIframe">
+                <label for ="name" class="form-label">お名前</label><br>
+                <input id ="name" class="form-label" type="text" name="entry.1502908570" placeholder="山田 太郎" required/><br>
+
+                <label for="email" class="form-label">メールアドレス</label><br>
+                <input id="email" class="form-label" type="text" name="entry.1033179079" placeholder="example@mail.co.jp" required/><br>
+
+                <label for="msg" class="form-label">お問い合わせ内容</label><br>
+                <textarea id ="msg" class="form-label" name="entry.1123612867" required></textarea><br>
+
+                <input class="form btn-success" type="button" name="button" value="送信" onclick="submitForm()"/><br>
+            </form>
+
+            <iframe name="dummyIframe" style="display: none;"></iframe>
             </div>
+
+            <div id="thxMessage" class="thxmsg-box" style="display: none;">
+                <p class="thxmsg">回答ありがとうございました。<br>
+                <a class="thxmsg" href="{{ url('/') }}">TOPへ戻る</a></p>
+            </div>
+            
+            <script>
+                function submitForm() {
+                document.gForm.submit();
+                document.getElementById('formWrapper').style.display='none';
+                document.getElementById('thxMessage').style.display='block';
+                }
+            </script>
+        </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\DogController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', [DogController::class, 'index'])->name('name');
-
-Route::get('/register', [DogController::class, 'register'])->name('register');
+Route::get('/', [DogController::class, 'index'])->name('index');
 
 Route::get('/dogprofile', [DogController::class, 'dogprofile'])->name('dogprofile');
 
